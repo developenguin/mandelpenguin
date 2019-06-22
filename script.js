25c60e32bb19d8652273f6428cba45bec7167d63
matthijs <matthijs@heavymetalpenguin.com>
2019-06-22 14:00:09 +0200

    Allow changing the values and make draw/reset buttons

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
 
-// Canvas and visual properties
+// Canvas and inputs
 const canvas = document.getElementById('mandelbrot');
 const ctx = canvas.getContext('2d');
 
-let zoom = 3000;
-let panX = 0.7;
-let panY = 0.6;
+const inputX = document.getElementById('xvalue');
+const inputY = document.getElementById('yvalue');
+const inputZoom = document.getElementById('zoom');
+
+const drawButton = document.getElementById('draw');
+const resetButton = document.getElementById('reset');
+
+// Position and zoom
+let zoom;
+let panX;
+let panY;
+
+// Respond to change
+inputX.addEventListener('change', (evt) => {
+  panX = evt.target.value;
+});
+
+inputY.addEventListener('change', (evt) => {
+  panY = evt.target.value;
+});
+
+inputZoom.addEventListener('change', (evt) => {
+  zoom = evt.target.value;
+});
+
+drawButton.addEventListener('click', () => {
+  drawCanvas();
+});
+
+resetButton.addEventListener('click', () => {
+  resetScreen();
+  drawCanvas();
+});
 
 // Computational constants
 const maxIterations = 1000;
+
+function resetScreen() {
+
+  inputX.value = 2;
+  inputY.value = 1.5;
+  inputZoom.value = 300;
+
+  zoom = inputZoom.value;
+  panX = inputX.value;
+  panY = inputY.value;
+
+}
 
 const drawPixel = (x, y, color) => {
   ctx.fillStyle = `hsl(${color.h},${color.s}%,${color.l}%`;
@@ -42,16 +84,23 @@ function calculateEscapeValue(x, y) {
 
 }
 
-for (let i = 0; i < canvas.width; i++) {
-  for (let j = 0; j < canvas.height; j++) {
+function drawCanvas() {
 
-    const escValue = calculateEscapeValue(i/zoom - panX, j/zoom - panY);
+  for (let i = 0; i < canvas.width; i++) {
+    for (let j = 0; j < canvas.height; j++) {
 
-    if (escValue === 0) {
-      drawPixel(i, j, { h: 0, s: 0, l: 0 });
-    } else {
-      drawPixel(i, j, { h: 0, s: 100, l: escValue });
+      const escValue = calculateEscapeValue(i/zoom - panX, j/zoom - panY);
+
+      if (escValue === 0) {
+        drawPixel(i, j, { h: 0, s: 0, l: 0 });
+      } else {
+        drawPixel(i, j, { h: 0, s: 100, l: escValue });
+      }
+
     }
-
   }
+
 }
+
+resetScreen();
+drawCanvas();
