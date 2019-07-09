fbe25f10c46c9f0022edb13cc0c09983dddfc2d6
developenguin <developenguin@users.noreply.github.com>
2019-07-09 11:59:08 +0200

    Both approaches should use the same color scheme

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ function resetScreen() {
 }
 
 const drawPixel = (x, y, color) => {
-  ctx.fillStyle = `hsl(${color.h},${color.s}%,${color.l}%`;
+  ctx.fillStyle = `rgba(${color[0]},${color[1]},${color[2]},${color[3]}`;
   ctx.fillRect(x, y, 1, 1);
 };
 
@@ -118,13 +118,39 @@ function calculateEscapeValue(x, y) {
 
     if ((cX * cY) > 2) {
       // Number is inside of the set if it has not escaped
-      return i / maxIterations * 100;
+      return i;
     }
 
   }
 
   // Number is not within the set if it has not escaped
   return 0;
+
+}
+
+function getColorForEscapeValue(options) {
+
+  const { maxIterations, escapeValue } = options;
+
+  if (escapeValue === maxIterations) {
+    return [0,0,0];  /* In the set. Assign black. */
+  } else if (escapeValue < maxIterations/64) {
+    return [escapeValue * 2, 0, 0, 255];    /* 0x0000 to 0x007E */
+  } else if (escapeValue < maxIterations/32) {
+    return [(((escapeValue - 64) * 128) / 126) + 128, 0, 0, 255];    /* 0x0080 to 0x00C0 */
+  } else if (escapeValue < maxIterations/16) {
+    return [(((escapeValue - 128) * 62) / 127) + 193, 0, 0, 255];    /* 0x00C1 to 0x00FF */
+  } else if (escapeValue < maxIterations/8) {
+    return [255, (((escapeValue - 256) * 62) / 255) + 1, 0, 255];    /* 0x01FF to 0x3FFF */
+  } else if (escapeValue < maxIterations/4) {
+    return [255, (((escapeValue - 512) * 63) / 511) + 64, 0, 255];   /* 0x40FF to 0x7FFF */
+  } else if (escapeValue < maxIterations/2) {
+    return [255, (((escapeValue - 1024) * 63) / 1023) + 128, 0, 255];   /* 0x80FF to 0xBFFF */
+  } else if (escapeValue < maxIterations) {
+    return [255, (((escapeValue - 2048) * 63) / 2047) + 192, 0, 255];   /* 0xC0FF to 0xFFFF */
+  } else {
+    return [255, 255, 0, 255];
+  }
 
 }
 
@@ -199,13 +225,14 @@ function drawCanvas() {
   for (let i = -canvasHalfWidth; i < canvasHalfWidth; i++) {
     for (let j = -canvasHalfHeight; j < canvasHalfHeight; j++) {
 
-      const escValue = calculateEscapeValue(i/zoom + middleX, j/zoom + middleY);
+      const escapeValue = calculateEscapeValue(i/zoom + middleX, j/zoom + middleY);
 
-      if (escValue === 0) {
-        drawPixel(i + canvasHalfWidth, j + canvasHalfHeight, { h: 0, s: 0, l: 0 });
-      } else {
-        drawPixel(i + canvasHalfWidth, j + canvasHalfHeight, { h: escValue * maxIterations % 360, s: 100, l: escValue });
-      }
+      const color = getColorForEscapeValue({
+        escapeValue,
+        maxIterations
+      });
+
+      drawPixel(i + canvasHalfWidth, j + canvasHalfHeight, color);
 
     }
   }
